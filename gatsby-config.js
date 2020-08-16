c217afe0e9879ecab83dc23ebb318eb20a907165
Steve <heystevegray@gmail.com>
2020-08-16 13:51:47 -0400

    Updated config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,6 +133,16 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-table-of-contents`,
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
