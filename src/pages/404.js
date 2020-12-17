668a33a74aeaa357f496df2ad8c2ebdf443c3016
Steve <heystevegray@gmail.com>
2020-12-16 19:48:05 -0500

    404 text.

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div className="hero-body mt-40 flex flex-col text-2xl text-white">
-      <h1>Whoops...</h1>
+      <h1 className="text-5xl">Whoops...</h1>
       <p className="mt-5">
         Um.. this is not the page you are looking for{" "}
         <span role="img" aria-label="hand wave">
